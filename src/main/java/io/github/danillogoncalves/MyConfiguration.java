77b417020444b48e6e555081a12b7071ba247a1c
Danillo <danillogoncalves001@gmail.com>
2024-06-24 21:49:56 -0300

    15. Configuração externalizada com application.properties

--- src/main/java/io/github/danillogoncalves/MyConfiguration.java
+++ src/main/java/io/github/danillogoncalves/MyConfiguration.java
@@ -6,14 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConfiguration {
 
-  @Bean(name = "applicationName")
-  public String applicationName() {
-    return "Sistema de Vendas";
-  }
-
-
-  @Bean(name = "otherConfiguration")
-  public String otherConfiguration() {
-    return "Sistema de Vendas";
-  }
 }
